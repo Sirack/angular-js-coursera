e6a519a00d708539c51a36f6ae481f6aaa2256fb
Sirack Hailu <sirackh@gmail.com>
2016-12-22 14:22:27 -0500

    submission

---
+++ module-1/app.js
@@ -0,0 +1,39 @@
+(function () {
+'use strict';
+
+angular.module('LunchApp', [])
+
+.controller('LunchController', function ($scope) {
+  $scope.message = ""; // default value
+  $scope.full_lunch = "";
+
+  $scope.process = function () {
+    var lunchs = $scope.full_lunch.split(',');
+
+    // preprocess values here
+    var realLunchs = []
+    var count = 0;
+    for(var i=0; i<lunchs.length; i++) {
+      if(lunchs[i].trim().length > 0) {
+        realLunchs[count] = lunchs[i];
+        count++;
+        console.log(realLunchs[count])
+      }
+    }
+
+    if(realLunchs.length == 0) {
+        $scope.message = "Please enter data first"
+    }
+    else {
+      if(realLunchs.length >= 3) {
+        $scope.message = "Too much!";
+      }
+      else if(realLunchs.length < 3) {
+        $scope.message = "Enjoy!";
+      }
+    }
+  };
+
+});
+
+})();
